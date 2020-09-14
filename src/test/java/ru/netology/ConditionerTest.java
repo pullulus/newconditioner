0f6adf39cde0d68c5d526f44ec1022c3a9346dd6
Elena Tsypileva <etsypileva@gmail.com>
2020-09-14 16:29:45 +0300

    added tests for currenttemperature

--- src/test/java/ru/netology/ConditionerTest.java
+++ src/test/java/ru/netology/ConditionerTest.java
@@ -6,6 +6,31 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ConditionerTest {
 
+    @Test
+    public void getAndSetCurrentTemperature() {
+        Conditioner conditioner = new Conditioner();
+
+        conditioner.setMaxTemperature(30);
+        conditioner.setCurrentTemperature(29);
+
+        int actual = conditioner.getCurrentTemperature();
+        int expected = 29;
+
+        assertEquals(expected,actual);
+    }
+
+@Test
+public void getAndSetCurrentTemperatureEqualMaximum() {
+    Conditioner conditioner = new Conditioner();
+
+    conditioner.setMaxTemperature(30);
+    conditioner.setCurrentTemperature(30);
+
+    int actual = conditioner.getCurrentTemperature();
+    int expected = 0;
+
+    assertEquals(expected, actual);
+}
 
     @Test
     public void increaseCurrentTemperature() {
